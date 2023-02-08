a4a0ffa1b68950501a1a258415701cd553aba638
Amogh Biniwal <amoghbiniwale04@gmail.com>
2023-02-09 00:19:41 +0530

    inheritance

---
+++ 21-Inheritance/src/com/aurionpro/model/child.java
@@ -0,0 +1,7 @@
+package com.aurionpro.model;
+
+public class child extends parent {
+	public child(){
+		System.out.println("inside child class");
+	}
+}
---
+++ 21-Inheritance/src/com/aurionpro/model/parent.java
@@ -0,0 +1,8 @@
+package com.aurionpro.model;
+
+public class parent {
+	public parent() {
+		System.out.println("inside parent class...");
+	}
+
+}
